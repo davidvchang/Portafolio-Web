e6f20ef2b5237728353fd0f4f3ae7fee7f72c6dc
David Valenzuela <dvalenzuelachang@gmail.com>
2024-03-14 11:25:45 -0700

    Se termino la navegacion

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import DV from '../assets/DV.webp'
 
 function Navegacion() {
   return (
     <nav>
-      <div className='flex justify-between flex-1'>
-        <div className='flex items-center flex-1'>
-          <span className='ml-10 p-2'>Logo</span>
+      <div className='lg:flex md-flex justify-between flex-1 p-1 w-screen'>
+        <div className='flex gap-16 items-center ms-16'>
+          <img src={DV} alt="DV Logo" className='p-2 w-20 h-20'/>
         </div>
 
-        <div className='lg:flex md-flex lg:flex-1 items center justify-between'>
+        <div className='lg:flex md-flex items center justify-between mr-48 mt-1'>
           <ul>
-              <li className='flex gap-10 p-1'>
-                  <a href="" className='p-2 hover:border-b-2'>Sobre mi</a>
-                  <a href="" className='p-2 hover:border-b-2'>Habilidades</a>
-                  <a href="" className='p-2 hover:border-b-2'>Proyectos</a>
-                  <a href="" className='p-2 hover:border-b-2'>Contactame</a>
+              <li className='flex gap-32 p-1'>
+                  <a href="" className='p-2 pb-1 hover:border-b-2'>Sobre mi</a>
+                  <a href="" className='p-2 pb-1 hover:border-b-2'>Habilidades</a>
+                  <a href="" className='p-2 pb-1 hover:border-b-2'>Proyectos</a>
+                  <a href="" className='p-2 pb-1 hover:border-b-2'>Contactame</a>
               </li>
           </ul>
         </div>
