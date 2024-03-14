06382c8a76021b01b3fca012759730a4736f3bd2
David Valenzuela <dvalenzuelachang@gmail.com>
2024-03-13 22:18:52 -0700

    Iniciando a hacer la navegacion de mi portafolio web

---
+++ src/components/Navegacion.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navegacion() {
+  return (
+    <nav>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <ul>
+            <li>
+                <a href="">Sobre mi</a>
+                <a href="">Habilidades</a>
+                <a href="">Proyectos</a>
+                <a href="">Contactame</a>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navegacion
