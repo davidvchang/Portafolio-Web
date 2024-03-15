386b34e5f340bee4d84b9a5cc128f96a70d261cb
David Valenzuela <dvalenzuelachang@gmail.com>
2024-03-14 22:46:18 -0700

    Nav terminado y responsivo

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -12,10 +12,10 @@ function Navegacion() {
         <div className='flex items-center mr-0 mt-1 justify-center  py-3 md:justify-between md:mr-32 '>
           <ul>
               <li className='flex flex-col gap-2 md:flex md:flex-row md:gap-12 md:p-1 lg:gap-20'>
-                  <a href="" className='hover:opacity-50 py-1 md:min-w-32 md:p-2 md:pb-1 md:hover:border-b-2  md:hover:-mb-1'>Sobre mi</a>
-                  <a href="" className='hover:opacity-50 py-1 md:p-2 md:pb-1 md:hover:border-b-2  md:hover:-mb-1'>Habilidades</a>
-                  <a href="" className='hover:opacity-50 py-1 md:min-w-24 md:p-2 md:pb-1 md:hover:border-b-2  md:hover:-mb-1'>Proyectos</a>
-                  <a href="" className='hover:opacity-50 py-1 md:min-w-28 md:p-2 md:pb-1 md:hover:border-b-2  md:hover:-mb-1'>Contactame</a>
+                  <a href="" className='hover:opacity-50 py-1 md:min-w-32 md:p-2 md:pb-1 md:hover:-mb-1'>Sobre mi</a>
+                  <a href="" className='hover:opacity-50 py-1 md:p-2 md:pb-1 md:hover:-mb-1'>Habilidades</a>
+                  <a href="" className='hover:opacity-50 py-1 md:min-w-24 md:p-2 md:pb-1 md:hover:-mb-1'>Proyectos</a>
+                  <a href="" className='hover:opacity-50 py-1 md:min-w-28 md:p-2 md:pb-1 md:hover:-mb-1'>Contactame</a>
               </li>
               
           </ul>
