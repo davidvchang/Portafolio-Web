dbf510e5ad313b21d9785adbf74ce2eef2266df8
David Valenzuela <dvalenzuelachang@gmail.com>
2024-03-20 19:26:46 -0700

    Se agregó el proyecto de Generador de usuarios mediante una api

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logos from '../Logos'
 import Captura_Portafolio from '../assets/Portafolio.png'
+import Randoms_Users from '../assets/Randoms_Users.png'
 
 function Proyectos() {
 
@@ -29,13 +30,24 @@ function Proyectos() {
                 </div>
 
             </div>
-            {/* <div className='flex flex-row w-full justify-center gap-1'>
-                <img src={Logos.Tailwind} alt="" className='w-6'/>
-                <img src={Logos.Tailwind} alt="" className='w-6'/>
-                <img src={Logos.Tailwind} alt="" className='w-6'/>
+        </div>
+
+        <div className='flex flex-col w-auto bg-slate-100 gap-5 shadow-2xl items-center overflow-hidden h-auto' onMouseEnter={MostrarHoverProyecto} onMouseLeave={MostrarHoverProyecto}>
+            <img src={Randoms_Users} alt="Generador de usuarios" className='h-64 w-96 min-w-96 shadow-2xl'/>
+            <div className='flex flex-col justify-center items-center absolute bg-slate-700 h-64 w-96 min-w-96 opacity-90 hidden shadow-2xl transition duration-1000 ease-in-out transform' id='proyecto-hover'>
+                    <a href="https://generador-usuarios-randoms.netlify.app/" target='_blank' className='w-6 h-6 absolute top-3 right-5 hover:opacity-55 hover:scale-105 hover:transition-all'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-slate-100">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                        </svg>
+                    </a>
+                <h3 className='text-slate-100 text-4xl'>Generador de Usuarios Randoms</h3>
+                <span className='text-slate-400 text-lg pb-3'>Este es un generador de usuarios al azar la cual carga lso datos desde una API, fue creado en React y Tailwind</span>
+                <div className='flex gap-5'>
+                    <img src={Logos.React} alt="React Logo" className='w-7 hover:scale-105 hover:transition-all'/>
+                    <img src={Logos.Tailwind} alt="Tailwind Logo" className='w-8 hover:scale-105 hover:transition-all'/>
+                </div>
+
             </div>
-            <h1>Portafolio Web</h1>
-            <span>Portafolio web creado en React con Tailwind</span> */}
         </div>
       </div>
     </section>
