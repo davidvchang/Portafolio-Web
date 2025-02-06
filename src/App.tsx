f84ea76c7406e14839cc643e0e6ca913bf3eeb00
davidvchang <dvalenzuelachang@gmail.com>
2025-02-05 23:04:52 -0700

    Add: functionality Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar"
 function App() {
 
   return (
-    <main className="bg-slate-50 w-[100vw] min-h-screen">
+    <main className="bg-slate-50 w-[100vw] min-h-screen dark:bg-[#101827]">
       <NavBar/>
       <Home/>
     </main>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,9 +3,14 @@ import Nav from './Nav'
 import { Moon } from 'lucide-react';
 
 const NavBar:React.FC = () => {
+
+  const toggleMode = () => {
+    document.documentElement.classList.toggle("dark");
+  }
+
   return (
-    <div className='w-full h-16 bg-white flex items-center justify-between px-5'>
-        <span className='text-4xl font-semibold text-blue-500'>DV</span>
+    <div className='w-full h-16 bg-white flex items-center justify-between px-5 fixed shadow'>
+        <span className='text-[44px] font-semibold text-blue-500'>DV</span>
 
         <div className='w-fit flex gap-10 items-center'>
             <Nav text='Inicio'/>
@@ -14,7 +19,7 @@ const NavBar:React.FC = () => {
             <Nav text='Experiencia'/>
             <Nav text='Contactame'/>
 
-            <button className='p-2 bg-slate-100 rounded cursor-pointer border border-slate-200 hover:bg-slate-200 hover:transition duration-300'><Moon className='w-5 h-5'/></button>
+            <button className='p-2 bg-slate-100 rounded cursor-pointer border border-slate-200 hover:bg-slate-200 hover:transition duration-300' onClick={toggleMode}><Moon className='w-5 h-5'/></button>
         </div>
     </div>
   )
