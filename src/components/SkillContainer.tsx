46e89f9113885bd64da8497d21f17504d3aee775
davidvchang <dvalenzuelachang@gmail.com>
2025-02-11 17:57:29 -0700

    Add: Skills section - Version PC

--- src/components/SkillContainer.tsx
+++ src/components/SkillContainer.tsx
@@ -6,7 +6,7 @@ interface PropsSkill{
 
 const SkillContainer:React.FC<PropsSkill> = ({ icon }) => {
   return (
-    <div className='flex w-20 h-40 rounded'>
+    <div className='flex w-20 h-20 justify-center items-center rounded-md border border-slate-300 bg-slate-200 dark:border-sky-900 dark:bg-[#1B2533]'>
         {icon}
     </div>
   )
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,11 +5,30 @@ import Icons from '../assets/icon.tsx'
 const Skills:React.FC = () => {
   return (
     <div className='flex flex-col w-full h-full items-center justify-center py-14'>
-        <div className='flex flex-col gap-14 w-[80%]'>
+        <div className='flex flex-col gap-14 w-[80%] dark:text-white'>
             <span className='text-3xl w-full text-center'>Habilidades</span>
 
-            <div className='flex flex-wrap gap-10'>
+            <div className='flex flex-wrap gap-10 justify-center'>
+                <SkillContainer icon={Icons.React}/>
+                <SkillContainer icon={Icons.Vue}/>
                 <SkillContainer icon={Icons.JS}/>
+                <SkillContainer icon={Icons.TypeScript}/>
+                <SkillContainer icon={Icons.HTML}/>
+                <SkillContainer icon={Icons.CSS}/>
+                <SkillContainer icon={Icons.Sass}/>
+                <SkillContainer icon={Icons.Tailwind}/>
+                <SkillContainer icon={Icons.Bootstrap}/>
+                <SkillContainer icon={Icons.Node}/>
+                <SkillContainer icon={Icons.Express}/>
+                <SkillContainer icon={Icons.MongoDB}/>
+                <SkillContainer icon={Icons.PostgreSQL}/>
+                <SkillContainer icon={Icons.SQL_Server}/>
+                <SkillContainer icon={Icons.MySQL}/>
+                <SkillContainer icon={Icons.Git}/>
+                <SkillContainer icon={Icons.GitHub}/>
+                <SkillContainer icon={Icons.SourceTree}/>
+                <SkillContainer icon={Icons.Python}/>
+                <SkillContainer icon={Icons.Postman}/>
             </div>
         
         </div>
