a54eed1d82498c881084ea23a1347a1fe6ceed62
davidvchang <dvalenzuelachang@gmail.com>
2025-02-12 23:13:36 -0700

    Change: size BtnProject

--- src/components/BtnProject.tsx
+++ src/components/BtnProject.tsx
@@ -8,7 +8,7 @@ interface PropsBtn {
 
 const BtnProject:React.FC<PropsBtn> = ({link, text, icon}) => {
   return (
-    <a href={link} target='_blank' className='flex items-center gap-2 px-3 py-2 border border-slate-300 rounded font-light bg-slate-50 hover:border-[#1D99EC] hover:bg-[#1D99EC] hover:text-white hover:transition duration-300 dark:bg-[#101827] dark:border-sky-900'>
+    <a href={link} target='_blank' className='flex items-center gap-2 px-3 py-3 md:px-3 md:py-2 border border-slate-300 rounded font-light bg-slate-50 hover:border-[#1D99EC] hover:bg-[#1D99EC] hover:text-white hover:transition duration-300 dark:bg-[#101827] dark:border-sky-900'>
         {icon}
         {text}
     </a>
